b3761e246f462d2b430eeea1f17d4daa0de19ea1
Iris <Irisnoer@gmeil.com>
2012-12-06 11:18:44 +0200

    added file, message and section annotations

--- indieportal/indieportal/src/domain/File.java
+++ indieportal/indieportal/src/domain/File.java
@@ -1,1 +1,46 @@
+package domain;
 
+
+import javax.persistence.Entity;
+import org.hibernate.validator.NotEmpty;
+import org.hibernate.validator.NotNull;
+
+@Entity
+public class File extends DomainObject{
+	
+	public enum FileType {Image, Video, Executable} 
+	@NotEmpty
+	String name;
+	@NotNull
+	FileType type;
+	
+	public File() {
+		
+	}
+	
+	
+	public FileType getType() {
+		return type;
+	}
+
+	public void setType(FileType type) {
+		this.type = type;
+	}
+
+	public File(String name) {
+		this.name = name;
+	}
+
+	public File(String name, FileType type) {
+		this.name = name;
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	};
+}
---
+++ indieportal/indieportal/src/domain/Message.java
@@ -0,0 +1,66 @@
+package domain;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+import org.hibernate.validator.NotNull;
+
+@Entity
+@NamedQueries({
+	@NamedQuery(name= "Theme.getByTheme", query= "SELECT c FROM Message c WHERE c.theme.id = ?1"),
+	@NamedQuery(name = "Theme.deleteAllByTheme", query = "DELETE FROM Message c WHERE c.theme.id = ?1")
+})
+public class Message extends Text{
+	
+	@NotNull
+	Theme theme;
+
+	Collection<File> files;
+	
+	public Message() {
+		files = new ArrayList<File>();
+	}
+	
+	public Message(String text,User user){
+		super(text,user);
+		files = new ArrayList<File>();
+	}
+	
+	public Theme getTheme() {
+		return theme;
+	}
+
+	public void setTheme(Theme theme) {
+		this.theme = theme;
+	}
+	
+	public void addFile(File file){
+		files.add(file);
+	};
+
+	public void deleteFile(File file){
+		files.remove(file);
+	};
+	
+	public Integer getNumberOfFiles(){
+		return files.size();
+	};
+	
+	public void increaseRating(){
+		setRating(getRating() + 1);
+	};
+	
+	public void decreaseRating(){
+		setRating(getRating() - 1);
+	}
+
+	public  Collection<File> getFiles() {
+		return files;
+	};
+	
+}
